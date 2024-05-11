7e770128ecc65beef01d561782c9015b59f704e2
Vinicius Simizu <viniciussimiz@gmail.com>
2024-05-11 12:23:56 -0300

    quicksort tested

--- lib/quicksort.c
+++ lib/quicksort.c
@@ -1,17 +1,17 @@
 #include <stdlib.h>
 #include "quicksort.h"
 
-void quicksort_swap(int *arr, unsigned int i, unsigned int j);
-void quicksort_choose_rand(int *arr, unsigned int i, unsigned int j);
+void quicksort_swap(int *arr, int i, int j);
+void quicksort_choose_rand(int *arr, int i, int j);
 
-unsigned int quicksort_hoare_partition(int *arr, unsigned int i, unsigned int j) {
+int quicksort_hoare_partition(int *arr, int i, int j) {
 	int pivot = j;
 	int left = i - 1;
 	int right = j;
 
 	while (1) {
-		do { ++left; } while (left < j && arr[left] <= pivot);
-		do { --right; } while (right > 0 && arr[right] > pivot);
+		do { ++left; } while (left < j && arr[left] <= arr[pivot]);
+		do { --right; } while (right > 0 && arr[right] > arr[pivot]);
 		if (left >= right) {
 			quicksort_swap(arr, left, j);
 			return left;
@@ -20,7 +20,7 @@ unsigned int quicksort_hoare_partition(int *arr, unsigned int i, unsigned int j)
 	};
 };
 
-void quicksort(int *arr, unsigned int i, unsigned int j) {
+void quicksort(int *arr, int i, int j) {
 	if (i < j) {
 		int idx = quicksort_hoare_partition(arr, i, j);
 		quicksort(arr, i, idx-1);
@@ -28,7 +28,7 @@ void quicksort(int *arr, unsigned int i, unsigned int j) {
 	};
 };
 
-void quicksort_randomized(int *arr, unsigned int i, unsigned int j) {
+void quicksort_randomized(int *arr, int i, int j) {
 	if (i < j) {
 		quicksort_choose_rand(arr, i, j);
 		int idx = quicksort_hoare_partition(arr, i, j);
@@ -37,7 +37,7 @@ void quicksort_randomized(int *arr, unsigned int i, unsigned int j) {
 	};
 };
 
-void quicksort_tre(int *arr, unsigned int i, unsigned int j) {
+void quicksort_tre(int *arr, int i, int j) {
 	while (i < j) {
 		int idx = quicksort_hoare_partition(arr, i, j);
 		quicksort_tre(arr, i, idx-1);
@@ -45,14 +45,14 @@ void quicksort_tre(int *arr, unsigned int i, unsigned int j) {
 	};
 };
 
-void quicksort_swap(int *arr, unsigned int i, unsigned int j) {
+void quicksort_swap(int *arr, int i, int j) {
 	int tmp = arr[i];
 	arr[i] = arr[j];
 	arr[j] = tmp;
 };
 
-void quicksort_choose_rand(int *arr, unsigned int i, unsigned int j) {
-	unsigned int size = j - i;
-	unsigned int idx = i + (size * rand() / RAND_MAX);
+void quicksort_choose_rand(int *arr, int i, int j) {
+	int size = j - i;
+	int idx = i + (size * rand() / RAND_MAX);
 	quicksort_swap(arr, idx, j);
 };
--- test/quicksort_test.c
+++ test/quicksort_test.c
@@ -4,23 +4,23 @@
 #include <string.h>
 #include "quicksort.h"
 
-struct vector {
+struct test_vector {
 	int *arr;
-	size_t size;
+	int size;
 };
 
-void assert_case(struct vector const *vect);
-struct vector *arrangement_case_empty();
-struct vector *arrangement_case_one_item();
-struct vector *arrangement_case_two_items();
-struct vector *arrangement_case_three_items();
-struct vector *arrangement_case_many_items();
-struct vector *create_arrangement(int *vect, size_t size);
-struct vector *vector_cpy(struct vector const *vect);
+void assert_case(struct test_vector const *vect);
+struct test_vector *arrangement_case_empty();
+struct test_vector *arrangement_case_one_item();
+struct test_vector *arrangement_case_two_items();
+struct test_vector *arrangement_case_three_items();
+struct test_vector *arrangement_case_many_items();
+struct test_vector *create_arrangement(int *vect, int size);
+struct test_vector *vector_cpy(struct test_vector const *vect);
+void test_free_vector(struct test_vector *vect);
 
 int main() {
-	size_t cases_size = 5;
-	struct vector *cases[] = {
+	struct test_vector *cases[] = {
 		arrangement_case_empty(),
 		arrangement_case_one_item(),
 		arrangement_case_two_items(),
@@ -28,63 +28,68 @@ int main() {
 		arrangement_case_many_items()
 	};
 
-	size_t acts_size = 3;
-	void *acts[] = {
+	void (*acts[])(int*, int, int) = {
 		quicksort,
 		quicksort_tre,
 		quicksort_randomized
 	};
 
+	int cases_size = sizeof(cases) / sizeof(void*);
+	int acts_size = sizeof(acts) / sizeof(void*);
+
 	for (int i = 0; i < acts_size; ++i) {
 		for (int j = 0; j < cases_size; ++j) {
-			struct vector const *case_ = cases[j];
+			struct test_vector const *case_ = cases[j];
+			struct test_vector *case_cpy = vector_cpy(case_);
 
-			struct vector *case_cpy = vector_cpy(case_);
+			(*acts[i])(case_cpy->arr, 0, case_cpy->size-1);
+
 			assert_case(case_cpy);
-			free(case_cpy);
+			test_free_vector(case_cpy);
 		};
 	}
 
 	// Free memory
-	for (size_t i = 0; i < cases_size; ++i) {
-		free(cases[i]);
+	for (int i = 0; i < cases_size; ++i) {
+		test_free_vector(cases[i]);
 	};
 
 	return 0;
 };
 
-void assert_case(struct vector const *vect) {
-	for (size_t i = 0; i < vect->size - 1; ++i) {
+void assert_case(struct test_vector const *vect) {
+	for (int i = 0; i < vect->size - 1; ++i) {
 		assert(vect->arr[i] <= vect->arr[i+1]);
 	};
 };
 
-struct vector *arrangement_case_empty() {
-	return create_arrangement(malloc(0), 0);
+struct test_vector *arrangement_case_empty() {
+	int arr[0] = {};
+	return create_arrangement(arr, 0);
 };
 
-struct vector *arrangement_case_one_item() {
+struct test_vector *arrangement_case_one_item() {
 	int arr[1] = {1};
 	return create_arrangement(arr, 1);
 };
 
-struct vector *arrangement_case_two_items() {
+struct test_vector *arrangement_case_two_items() {
 	int arr[2] = {2, -5};
 	return create_arrangement(arr, 2);
 };
 
-struct vector *arrangement_case_three_items() {
+struct test_vector *arrangement_case_three_items() {
 	int arr[3] = {2, -5, 3};
 	return create_arrangement(arr, 3);
 };
 
-struct vector *arrangement_case_many_items() {
+struct test_vector *arrangement_case_many_items() {
 	int arr[10] = {2, 1, 45, 2, 6, 2, 3, 5, 1, 0};
 	return create_arrangement(arr, 10);
 };
 
-struct vector *create_arrangement(int *vect, size_t size) {
-	struct vector *arrange = malloc(sizeof(struct vector));
+struct test_vector *create_arrangement(int *vect, int size) {
+	struct test_vector *arrange = malloc(sizeof(struct test_vector));
 	arrange->size = size;
 	arrange->arr = malloc(size * sizeof(int));
 	for (int i = 0; i < size; ++i) {
@@ -94,10 +99,15 @@ struct vector *create_arrangement(int *vect, size_t size) {
 	return arrange;
 };
 
-struct vector *vector_cpy(struct vector const *vect) {
-	struct vector *vect_cpy = malloc(sizeof(struct vector));
+struct test_vector *vector_cpy(struct test_vector const *vect) {
+	struct test_vector *vect_cpy = malloc(sizeof(struct test_vector));
 	vect_cpy->size = vect->size;
 	vect_cpy->arr = malloc(vect->size * sizeof(int));
 	memcpy(vect_cpy->arr, vect->arr, vect->size * sizeof(int));
 	return vect_cpy;
+};
+
+void test_free_vector(struct test_vector *vect) {
+	free(vect->arr);
+	free(vect);
 };
